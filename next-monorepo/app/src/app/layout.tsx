ffff421792c4016434b36d7c0da764a282bfd620
ramonliao <yc52811@hotmail.com>
2026-03-22 23:54:23 +0800

    feat: wire AppShell into layout, add PageHeader, remove ShellFrame

--- next-monorepo/app/src/app/layout.tsx
+++ next-monorepo/app/src/app/layout.tsx
@@ -1,12 +1,15 @@
 import "@mysten/dapp-kit/dist/index.css";
 import "./globals.css";
 import { Providers } from "./providers";
+import { AppShell } from "@/components/AppShell";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
-        <Providers>{children}</Providers>
+        <Providers>
+          <AppShell>{children}</AppShell>
+        </Providers>
       </body>
     </html>
   );
